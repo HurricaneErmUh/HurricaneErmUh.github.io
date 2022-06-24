befa1cc76f525ac41a02f4140848993d1e7f3491
HurricaneErmUh <108150460+HurricaneErmUh@users.noreply.github.com>
2022-06-24 12:11:24 -0700

    Update hiddenjsreader.js

--- hiddenjsreader.js
+++ hiddenjsreader.js
@@ -1,25 +1,29 @@
 var sectionnumber = 0
 var paranumber = 0
 
-var prevs = document.createElement("button")
-prevs.innerText = "<<"
-prevs.setAttribute("onclick", "sectionnumber--; setText()")
-document.body.appendChild(prevs)
-
-var nexts = document.createElement("button")
-nexts.innerText = ">>"
-nexts.setAttribute("onclick", "sectionnumber++; setText()")
-document.body.appendChild(nexts)
+var nextp = document.createElement("button")
+nextp.innerText = ">"
+nextp.setAttribute("onclick", "paranumber++; setText()")
+//document.body.appendChild(nextp)
+document.getElementById("readerelement").parentElement.insertBefore(nextp, document.getElementById("readerelement").nextSibling)
 
 var prevp = document.createElement("button")
 prevp.innerText = "<"
 prevp.setAttribute("onclick", "paranumber--; setText()")
-document.body.appendChild(prevp)
+//document.body.appendChild(prevp)
+document.getElementById("readerelement").parentElement.insertBefore(prevp, document.getElementById("readerelement").nextSibling)
 
-var nextp = document.createElement("button")
-nextp.innerText = ">"
-nextp.setAttribute("onclick", "paranumber++; setText()")
-document.body.appendChild(nextp)
+var nexts = document.createElement("button")
+nexts.innerText = ">>"
+nexts.setAttribute("onclick", "sectionnumber++; paranumber = 0; setText()")
+//document.body.appendChild(nexts)
+document.getElementById("readerelement").parentElement.insertBefore(nexts, document.getElementById("readerelement").nextSibling)
+
+var prevs = document.createElement("button")
+prevs.innerText = "<<"
+prevs.setAttribute("onclick", "sectionnumber--; paranumber = 0; setText()")
+//document.body.appendChild(prevs)
+document.getElementById("readerelement").parentElement.insertBefore(prevs, document.getElementById("readerelement").nextSibling)
 
 var book = document.createElement("book")
 setBook("https://standardebooks.org/ebooks/henryk-sienkiewicz/the-deluge/jeremiah-curtin/text/single-page")
